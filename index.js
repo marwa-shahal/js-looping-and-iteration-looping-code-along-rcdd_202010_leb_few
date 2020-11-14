7de90314b984ffc8b4c25101aad3a72b5a00293a
binding-event-8377 <marwa.al.shahal@gmail.com>
2020-11-14 11:00:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+//for loop
+const cards=['Ada', 'Brendan', 'Ali'];
+const birth='birthday';
+
+
+
+
+function writecards(cards,event)  {
+  for ( let i=0; i< cards.length ; i++) 
+    { console.log (`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);}
+    return ;}
+
+
+//while loop
+function CountDown(i) {
+  let CountDownVar=i ;
+  while (CountDownVar >=0) {
+    console.log (CountDownVar);
+    CountDownVar=CountDownVar-1;}
+  return; }

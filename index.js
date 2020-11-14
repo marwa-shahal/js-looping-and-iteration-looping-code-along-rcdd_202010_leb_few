b87d349d1db5ede3a0b09764cf1c729e35ac764a
binding-event-8377 <marwa.al.shahal@gmail.com>
2020-11-14 11:22:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const cards=['Ada', 'Brendan', 'Ali'];
 const birth='birthday';
 
 
-
+writecards(cards,birth);
 
 function writecards(array,keyword)  { 
   const messages=[];
   for ( let i=0; i< cards.length ; i++) 
-    { messages.push (`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);}
+    { messages.push (`Thank you, ${cards[i]}, for the wonderful ${keyword} gift!`);}
     return messages;}
 
 
 //while loop
+CountDown(10);
+ 
 function CountDown(i) {
   let CountDownVar=i ;
   while (CountDownVar >=0) {

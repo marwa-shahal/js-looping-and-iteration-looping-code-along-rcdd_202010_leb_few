32d68a45dc6ed629a0be54ef1844cd244ba48e7c
binding-event-8377 <marwa.al.shahal@gmail.com>
2020-11-14 11:49:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+//for loop
+
+
+
+function writeCards(array, keyword) {
+const messages = [];
+  for (let i = 0; i < array.length; i++) {
+    messages.push (`Thank you, ${array[i]}, for the wonderful ${keyword} gift!`);
+  }
+  return messages;
+}
+
+
+//while loop
+
+ function countDown(i){
+  while(i>=0){
+    console.log(i--);
+  }
+}
+countDown(10);
